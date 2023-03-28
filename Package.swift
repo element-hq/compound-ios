0906871951090eccbeba998e45fd6f0b46484e49
Doug <douglase@element.io>
2023-03-28 14:23:53 +0100

    Add search field style.

--- Package.swift
+++ Package.swift
@@ -10,11 +10,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vector-im/compound-design-tokens.git", revision: "342145ff8044b58b967186b0efe34477e1f7c3ca"),
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.2.3")
     ],
     targets: [
         .target(name: "Compound",
                 dependencies: [
                     .product(name: "CompoundDesignTokens", package: "compound-design-tokens"),
+                    .product(name: "Introspect", package: "SwiftUI-Introspect")
                 ]),
         .testTarget(name: "CompoundTests", dependencies: ["Compound"])
     ]
