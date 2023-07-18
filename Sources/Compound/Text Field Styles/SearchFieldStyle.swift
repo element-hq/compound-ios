b9e9cff9f36d11b4020799c15d635a5da908c551
Doug <douglase@element.io>
2023-07-18 15:16:54 +0100

    Keep navigation bar visible when searching in a modal.
    Update Introspect.

--- Sources/Compound/Text Field Styles/SearchFieldStyle.swift
+++ Sources/Compound/Text Field Styles/SearchFieldStyle.swift
@@ -14,16 +14,24 @@
 // limitations under the License.
 //
 
-import Introspect
 import SwiftUI
+import SwiftUIIntrospect
 
 public extension View {
     /// Styles a search bar text field using the Compound design tokens.
     /// This modifier is to be used in combination with `.searchable`.
     func compoundSearchField() -> some View {
-        // Ported from Riot iOS as this is the only reliable way to get the exact look we want.
-        // However this is fragile and tied to gutwrenching the current UISearchBar internals.
-        introspectSearchController { searchController in
+        introspect(.navigationStack, on: .iOS(.v16), scope: .ancestor) { navigationController in
+            // Uses the navigation stack as .searchField is unreliable when pushing the second search bar, during the create rooms flow.
+            guard let searchController = navigationController.navigationBar.topItem?.searchController else { return }
+            
+            // When the search field is part of a navigation bar on a modal, keep the whole bar visible when searching
+            if navigationController.presentingViewController != nil {
+                searchController.hidesNavigationBarDuringPresentation = false
+            }
+            
+            // Ported from Riot iOS as this is the only reliable way to get the exact look we want.
+            // However this is fragile and tied to gutwrenching the current UISearchBar internals.
             let textColor = UIColor(.compound.textPrimary)
             let placeholderColor = UIColor(.compound.textPlaceholder)
             let textFieldTintColor = UIColor(.compound.iconAccentTertiary)
