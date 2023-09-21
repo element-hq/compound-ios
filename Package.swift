b2c3e7b5d9a775b9594a8b452d17755b1befd408
Mauro Romito <mauro.romito@element.io>
2023-09-21 14:59:29 +0200

    testing and playbook setup done

--- Package.swift
+++ Package.swift
@@ -11,15 +11,32 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vector-im/compound-design-tokens.git", revision: "387d2b7211f07761c67e849c59414a1bb803defa"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.9.0"),
-        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1")
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1"),
+        .package(url: "https://github.com/BarredEwe/Prefire", from: "1.5.0"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.13.0")
     ],
     targets: [
-        .target(name: "Compound",
-                dependencies: [
-                    .product(name: "CompoundDesignTokens", package: "compound-design-tokens"),
-                    .product(name: "SwiftUIIntrospect", package: "SwiftUI-Introspect"),
-                    .product(name: "SFSafeSymbols", package: "SFSafeSymbols")
-                ]),
-        .testTarget(name: "CompoundTests", dependencies: ["Compound"])
+        .target(
+            name: "Compound",
+            dependencies: [
+                .product(name: "CompoundDesignTokens", package: "compound-design-tokens"),
+                .product(name: "SwiftUIIntrospect", package: "SwiftUI-Introspect"),
+                .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
+                .product(name: "Prefire", package: "Prefire")
+            ],
+            plugins: [
+                .plugin(name: "PrefirePlaybookPlugin", package: "Prefire")
+            ]
+        ),
+        .testTarget(
+            name: "CompoundTests",
+            dependencies: [
+                "Compound",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
+            plugins: [
+                .plugin(name: "PrefireTestsPlugin", package: "Prefire")
+            ]
+        )
     ]
 )
