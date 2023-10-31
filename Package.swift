34dcd1e2960f3b0b7f50d6e7d020c108d5acf604
Alfonso Grillo <alfogrillo@element.io>
2023-10-31 16:47:43 +0100

    Update compound icons (#41)
    * Update compound icons
    
    * Rename icons
    
    * Update more icons

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Compound", targets: ["Compound"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vector-im/compound-design-tokens.git", revision: "387d2b7211f07761c67e849c59414a1bb803defa"),
+        .package(url: "https://github.com/vector-im/compound-design-tokens.git", from: "0.0.7"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.9.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1"),
         .package(url: "https://github.com/BarredEwe/Prefire", from: "1.5.0"),
