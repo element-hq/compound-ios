e0d845a6af2dc35a3b8ad4c00061dd4e85f42427
Mauro Romito <mauro.romito@element.io>
2025-09-12 15:46:24 +0200

    update compound min version and introspect

--- Sources/Compound/Extensions/PlatformVersionPredicate.swift
+++ Sources/Compound/Extensions/PlatformVersionPredicate.swift
@@ -10,36 +10,36 @@ import SwiftUI
 
 public extension PlatformViewVersionPredicate<NavigationStackType, UINavigationController> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
 
 public extension PlatformViewVersionPredicate<WindowType, UIWindow> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
 
 public extension PlatformViewVersionPredicate<TextFieldType, UITextField> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
 
 public extension PlatformViewVersionPredicate<ScrollViewType, UIScrollView> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
 
 public extension PlatformViewVersionPredicate<ViewControllerType, UIViewController> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
 
 public extension PlatformViewVersionPredicate<TabViewType, UITabBarController> {
     static var supportedVersions: Self {
-        .iOS(.v16...)
+        .iOS(.v17...)
     }
 }
